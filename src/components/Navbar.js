9d2da9e7053b53233241def3061adc35318e6bf6
PhillipH <u1309229@utah.edu>
2023-12-06 19:32:23 -0700

    added Menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Navbar.css"
+import { Menu, MenuButton, MenuItem} from '@aws-amplify/ui-react';
 // import { signOut } from '@aws-amplify/auth';
 
 
@@ -7,11 +8,19 @@ function Navbar(signOut, user){
     return (
         <div className="wrapper">
             <nav className="nav">
-                <a className="site-title" href="/">MedCapture</a>
+                {/* <a className="site-title" href="/">MedCapture</a> */}
+                <Menu
+                    trigger={
+                    <MenuButton variation="primary" size="medium" width="10%">
+                        MedCapture
+                    </MenuButton>
+                    }
+                >
+                    <MenuItem as="a" href="/" color={"black"}> Home </MenuItem>
+                    <MenuItem as="a" href="/examination" color={"black"}> Examination </MenuItem>
+                    <MenuItem as="a" href="#" color={"black"}> View PDF </MenuItem>
+                </Menu>
                 <ul>
-                    <li>
-                        <a href="/examination">Examination</a>
-                    </li>
                     <li>
                         <a href="./">Hello {signOut.user?.attributes.email}</a>
                     </li>
