fb531d8592ee5d150c61fa9e25db9962551d758d
springborn-ryan <90922425+springborn-ryan@users.noreply.github.com>
2023-12-07 19:15:25 -0700

    saving diagnosis work in progress

--- src/components/DiagnosisList.jsx
+++ src/components/DiagnosisList.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import './DiagnosisList.css'
+import { useState } from 'react'
 
 function DiagnosisList() {
     const options = [
-        {label: "item 0"},
-        {label: "item 1"},
-        {label: "item 2"},
-        {label: "item 3"},
-        {label: "item 4"},
-        {label: "item 5"},
-        {label: "item 6"},
-        {label: "item 7"},
-        {label: "item 8"},
-        {label: "item 9"}
+        {label: "item 0", value: 0},
+        {label: "item 1", value: 1},
+        {label: "item 2", value: 2},
+        {label: "item 3", value: 3},
+        {label: "item 4", value: 4},
+        {label: "item 5", value: 5},
+        {label: "item 6", value: 6},
+        {label: "item 7", value: 7},
+        {label: "item 8", value: 8},
+        {label: "item 9", value: 9}
     ]
+
+    const [diagnosis, setDiagnosis] = useState(1)
     
     return (
         <div className= "dropdown">
-            <select className= "form-select">
+            <select className= "form-select" value = {diagnosis} onChange={e => setDiagnosis(e.target.value)}>
                 {options.map(option => (
                     <option value= {option.value}>{option.label}</option>
                 ))}
--- src/components/DiagnosisPopup.jsx
+++ src/components/DiagnosisPopup.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import './DiagnosisPopup.css'
 import DiagnosisList from "./DiagnosisList"
+import { useState } from "react"
 
 function DiagnosisPopup(props) {
   // console.log(props.delete_circle)
   // console.log(props.circle_key)
+  const [comments, setComments] = useState('sample comment')
   return ( props.trigger) ? (
     <div className="popup">
       <div className="popup-inner">
         <h3>Select Diagnosis</h3>
         <DiagnosisList/>
         <h3>Comments</h3>
-        <textarea name = "comment" type = "text" id = "comment"></textarea>
+        <textarea name = "comment" type = "text" id = "comment" value = {comments} onChange={e => setComments(e.target.value)}></textarea>
         <br/>
         <br/>
         <button className="done-button" onClick= {() => props.setTrigger(false)}>Done</button>
