30b9c18bb9cc30c52d625f3847f8eb5457089d1d
Tanner Jones <tannerjones1701@gmail.com>
2024-02-23 09:10:43 -0700

    schema update - amplify pull

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,22 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "EMRApp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "EMRApp": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
